f700b64b39d7c2348bbacfa40c606879789e1c92
Prabhanshu Ranjan <prabme3@gmail.com>
2026-07-25 23:07:24 +0530

    Create server header file and document server basics

--- include/server/Server.h
+++ include/server/Server.h
@@ -1,9 +1,17 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+
 class Server {
 public:
+    Server();
+    ~Server();
+
     void start();
+private:
+    int serverSocket;
 };
+
+Server::Server() : serverSocket(-1) {}
 
 #endif
