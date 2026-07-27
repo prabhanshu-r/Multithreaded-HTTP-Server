b3ac7a80bf0238fa60246440414a6b9dd97a4e01
Prabhanshu <prabme3@gmail.com>
2026-07-27 17:57:12 +0000

    remove build directory from repo

--- build/CMakeFiles/MultithreadedHTTPServer.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/MultithreadedHTTPServer.dir/src/main.cpp.o: \
- /home/prabhanshu/projects/Multithreaded-HTTP-Server/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/prabhanshu/projects/Multithreaded-HTTP-Server/include/server/Server.hpp
